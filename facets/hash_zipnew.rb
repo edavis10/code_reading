4c528eeeedf7f5f593fb49bae504b90db61e7b97
Eric Davis <edavis@littlestreamsoftware.com>
2010-05-25 08:07:38 -0700

    Added an alternative implementation for Hash#zipnew

--- facets/hash_zipnew.rb
+++ facets/hash_zipnew.rb
@@ -4,6 +4,16 @@
 require '../base'
 require "facets/hash/zipnew"
 
+# Alternative implementation
+class Hash
+  def self.alt_zipnew(keys, values)
+    h = {}
+    keys.each_with_index {|key, index| h[ key ] = values[ index ] }
+    h
+  end
+end
+
+
 class HashZipnew
   def data
     keys = [
@@ -22,9 +32,28 @@ class HashZipnew
              ]
     Hash.zipnew(keys, values)
   end
+
+  def alternative_implementation
+    keys = [
+            '2010-05-15',
+            '2010-05-16',
+            '2010-05-17',
+            '2010-05-18',
+            '2010-05-19'
+            ]
+    values = [
+              100.0,
+              450.5,
+              89.0,
+              0.0,
+              78.5
+             ]
+    Hash.alt_zipnew(keys, values)
+  end
 end
 
 ap(HashZipnew.new.data)
+ap(HashZipnew.new.alternative_implementation)
 
 class HashZipnewTest < Test::Unit::TestCase
   def test_use_the_arrays_to_map_keys_to_values
@@ -36,5 +65,14 @@ class HashZipnewTest < Test::Unit::TestCase
     assert_equal   0.0, hashish['2010-05-18']
     assert_equal  78.5, hashish['2010-05-19']
   end
-  
+
+  def test_alternative_implementation
+    hashish = HashZipnew.new.alternative_implementation
+
+    assert_equal 100.0, hashish['2010-05-15']
+    assert_equal 450.5, hashish['2010-05-16']
+    assert_equal  89.0, hashish['2010-05-17']
+    assert_equal   0.0, hashish['2010-05-18']
+    assert_equal  78.5, hashish['2010-05-19']
+  end
 end
