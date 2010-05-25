336153aad98f0f6762d0d3a0d1f10071fd10e195
Eric Davis <edavis@littlestreamsoftware.com>
2010-05-25 07:54:47 -0700

    Code Reading 2, Hash#zipnew

---
+++ facets/hash_zipnew.rb
@@ -0,0 +1,40 @@
+#!/usr/bin/ruby -wKU
+#
+# Code Reading #2
+require '../base'
+require "facets/hash/zipnew"
+
+class HashZipnew
+  def data
+    keys = [
+            '2010-05-15',
+            '2010-05-16',
+            '2010-05-17',
+            '2010-05-18',
+            '2010-05-19'
+            ]
+    values = [
+              100.0,
+              450.5,
+              89.0,
+              0.0,
+              78.5
+             ]
+    Hash.zipnew(keys, values)
+  end
+end
+
+ap(HashZipnew.new.data)
+
+class HashZipnewTest < Test::Unit::TestCase
+  def test_use_the_arrays_to_map_keys_to_values
+    hashish = HashZipnew.new.data
+
+    assert_equal 100.0, hashish['2010-05-15']
+    assert_equal 450.5, hashish['2010-05-16']
+    assert_equal  89.0, hashish['2010-05-17']
+    assert_equal   0.0, hashish['2010-05-18']
+    assert_equal  78.5, hashish['2010-05-19']
+  end
+  
+end
